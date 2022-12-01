55a92e80680022bf008e90d999c803a7550b99ac
Chidimma <chidimmamofodum@gmail.com>
2022-12-01 23:47:31 +0000

    basic layout and functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+
+  const [userName, setUserName] = useState("")
+   const [password, setPassword] = useState("");
+
+   function updatePassword(event) {
+    setPassword(event.target.value)
+   }
+
+    function updateUserName(event) {
+      setUserName(event.target.value);
+    }
+
+    function isVisible() {
+     return (userName !== "" && password !== "") ? false: true 
+    }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type = "text" onChange = {updateUserName} value = {userName} />
+     <input type = "password" onChange = {updatePassword} value = {password} />
+     <button disabled = {isVisible()}>Submit</button>
     </div>
   );
 }
